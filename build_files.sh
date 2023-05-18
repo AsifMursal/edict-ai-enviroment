8c4d9c1643efc03e978908cf1c5babc09cf95cfa
AsifMursal <97343798+AsifMursal@users.noreply.github.com>
2023-05-18 17:32:29 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
  echo "BUILD START"
- python3.9 -m pip3 install db-sqlite3
- pip3 install wheel setuptools pip --upgrade
- python3.9 -m pip install -r requirements.txt
+
+ python3.9 -m pip3 install wheel setuptools pip --upgrade
+  python3.9 -m pip3 install db-sqlite3
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
