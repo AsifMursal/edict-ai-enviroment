c44a9fdeea4ab659c2c9e055ac55ef8d654106c3
AsifMursal <97343798+AsifMursal@users.noreply.github.com>
2023-05-18 16:57:53 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  echo "BUILD START"
- python3.9 -m pip install pysqlite3 
+ python3.9 -m pip install db-sqlite3
  python3.9 -m pip install -r requirements.txt
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
