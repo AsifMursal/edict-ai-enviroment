7e6eea31866137d53f0cd57abb4ce7cbd4f30845
AsifMursal <97343798+AsifMursal@users.noreply.github.com>
2023-05-18 21:39:22 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
  echo "BUILD START"
 
-
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
